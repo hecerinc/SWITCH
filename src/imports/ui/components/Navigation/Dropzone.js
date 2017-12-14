9fae793a2c8c36f13df786c3a609d0b6b0e89b64
Hector Rincon <hecerinc@hotmail.com>
2017-12-13 18:44:58 -0800

    Rename files, remove trash & add docs for data architecture

--- src/imports/ui/components/Navigation/Dropzone.js
+++ src/imports/ui/components/Navigation/Dropzone.js
@@ -5,162 +5,162 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 let componentConfig = {
-  iconFiletypes: ['.csv', '.tab', '.xlsx'],
-  showFiletypeIcon: true,
-  postUrl: 'no-url',
+	iconFiletypes: ['.csv', '.tab', '.xlsx'],
+	showFiletypeIcon: true,
+	postUrl: 'no-url',
 };
 
 var djsConfig = {
-  acceptedFiles: '.csv,.tab,.xlsx,.xls',
-  autoProcessQueue: false,
-  showFiletypeIcon: true,
-  params: {
-    myParam: 'Hello from a parameter!',
-    dictRemoveFile: 'lol',
-    anotherParam: 43,
-  },
-  previewTemplate: ReactDOMServer.renderToStaticMarkup(
-    <div className="dz-preview dz-file-preview">
-      <div className="uploaded-files">
-        <div className="dz-details">
-          <div className="dz-file-description">
-            <div className="dz-filename">
-              <span data-dz-name="true" />
-            </div>
-            <div className="dz-remove dz-remove-icon" data-dz-remove>
-              <img src={'./assets/icons/delete.png'} alt="" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  ),
+	acceptedFiles: '.csv,.tab,.xlsx,.xls,.tsv',
+	autoProcessQueue: false,
+	showFiletypeIcon: true,
+	params: {
+		myParam: 'Hello from a parameter!',
+		dictRemoveFile: 'lol',
+		anotherParam: 43,
+	},
+	previewTemplate: ReactDOMServer.renderToStaticMarkup(
+		<div className="dz-preview dz-file-preview">
+			<div className="uploaded-files">
+				<div className="dz-details">
+					<div className="dz-file-description">
+						<div className="dz-filename">
+							<span data-dz-name="true" />
+						</div>
+						<div className="dz-remove dz-remove-icon" data-dz-remove>
+							<img src={'./assets/icons/delete.png'} alt="" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	),
 };
 
 const Dropzone = props => {
-  var eventHandlers = {
-    addedfile: file => {
-      switch (file.name) {
-        case 'PowerPlants.csv': {
-          props.handlePowerPlants(file);
-          break;
-        }
-        case 'project_info.tab': {
-          props.handleProjectInfo(file);
-          break;
-        }
-        case 'BuildTrans.tab': {
-          props.handleTransmissionLines(file);
-          break;
-        }
+	var eventHandlers = {
+		addedfile: file => {
+			switch (file.name) {
+				case 'PowerPlants.csv': {
+					props.handlePowerPlants(file);
+					break;
+				}
+				case 'project_info.tab': {
+					props.handleProjectInfo(file);
+					break;
+				}
+				case 'BuildTrans.tab': {
+					props.handleTransmissionLines(file);
+					break;
+				}
 
-        default:
-          break;
-      }
-    },
-  };
-  return (
-    <div>
-      <DropzoneComponent
-        config={componentConfig}
-        eventHandlers={eventHandlers}
-        djsConfig={djsConfig}
-      />
-    </div>
-  );
+				default:
+					break;
+			}
+		},
+	};
+	return (
+		<div>
+			<DropzoneComponent
+				config={componentConfig}
+				eventHandlers={eventHandlers}
+				djsConfig={djsConfig}
+			/>
+		</div>
+	);
 };
 
 const DropzoneContainer = compose(
-  graphql(
-    gql`
-      mutation uploadTL($file: FileInput) {
-        uploadTransmissionLines(file: $file)
-      }
-    `,
-    {
-      name: 'uploadTL',
-      options: {
-        update: (proxy, { data: { uploadTransmissionLines } }) => {
-          // const data = proxy.readQuery({ query });
-          // console.log(data, uploadTransmissionLines);
-          // data.files.push(uploadFile);          FIXME
-          // proxy.writeQuery({ query, data });
-        },
-      },
-    }
-  ),
-  graphql(
-    gql`
-      mutation uploadPP($file: FileInput) {
-        uploadPowerPlants(file: $file)
-      }
-    `,
-    {
-      name: 'uploadPP',
-    }
-  ),
-  graphql(
-    gql`
-      mutation uploadPI($file: FileInput) {
-        uploadProjectInfo(file: $file)
-      }
-    `,
-    {
-      name: 'uploadPI',
-      options: {
-        update: (proxy, { data: { uploadProjectInfo } }) => {
-          // const data = proxy.readQuery({ query });
-          // console.log(data, uploadLoadZones);
-          // data.files.push(uploadFile);          FIXME
-          // proxy.writeQuery({ query, data });
-        },
-      },
-    }
-  ),
-  withHandlers({
-    handleTransmissionLines: ({ uploadTL }) => file => {
-      uploadTL({
-        variables: { file },
-        refetchQueries: [
-          {
-            query: gql`
-              query fileQuery {
-                getTransmissionLines
-              }
-            `,
-          },
-        ],
-      });
-    },
-    handlePowerPlants: ({ uploadPP }) => file => {
-      uploadPP({
-        variables: { file },
-        refetchQueries: [
-          {
-            query: gql`
-              query getPP {
-                getPowerPlants
-              }
-            `,
-          },
-        ],
-      });
-    },
-    handleProjectInfo: ({ uploadPI }) => file => {
-      uploadPI({
-        variables: { file },
-        refetchQueries: [
-          {
-            query: gql`
-              query getPI {
-                getProjectInfo
-              }
-            `,
-          },
-        ],
-      });
-    },
-  })
+	graphql(
+		gql`
+			mutation uploadTL($file: FileInput) {
+				uploadTransmissionLines(file: $file)
+			}
+		`,
+		{
+			name: 'uploadTL',
+			options: {
+				update: (proxy, { data: { uploadTransmissionLines } }) => {
+					// const data = proxy.readQuery({ query });
+					// console.log(data, uploadTransmissionLines);
+					// data.files.push(uploadFile);          FIXME
+					// proxy.writeQuery({ query, data });
+				},
+			},
+		}
+	),
+	graphql(
+		gql`
+			mutation uploadPP($file: FileInput) {
+				uploadPowerPlants(file: $file)
+			}
+		`,
+		{
+			name: 'uploadPP',
+		}
+	),
+	graphql(
+		gql`
+			mutation uploadPI($file: FileInput) {
+				uploadProjectInfo(file: $file)
+			}
+		`,
+		{
+			name: 'uploadPI',
+			options: {
+				update: (proxy, { data: { uploadProjectInfo } }) => {
+					// const data = proxy.readQuery({ query });
+					// console.log(data, uploadLoadZones);
+					// data.files.push(uploadFile);          FIXME
+					// proxy.writeQuery({ query, data });
+				},
+			},
+		}
+	),
+	withHandlers({
+		handleTransmissionLines: ({ uploadTL }) => file => {
+			uploadTL({
+				variables: { file },
+				refetchQueries: [
+					{
+						query: gql`
+							query fileQuery {
+								getTransmissionLines
+							}
+						`,
+					},
+				],
+			});
+		},
+		handlePowerPlants: ({ uploadPP }) => file => {
+			uploadPP({
+				variables: { file },
+				refetchQueries: [
+					{
+						query: gql`
+							query getPP {
+								getPowerPlants
+							}
+						`,
+					},
+				],
+			});
+		},
+		handleProjectInfo: ({ uploadPI }) => file => {
+			uploadPI({
+				variables: { file },
+				refetchQueries: [
+					{
+						query: gql`
+							query getPI {
+								getProjectInfo
+							}
+						`,
+					},
+				],
+			});
+		},
+	})
 )(Dropzone);
 
 export default DropzoneContainer;
--- src/todos.md
+++ src/todos.md
@@ -1,5 +1,5 @@
 - [] Rename all of the important files starting with dot and remove the dot (move these files to a location such that Meteor does not import them)
-
+- [] Accept `.tsv` files (prefer from `.tab`)
 
 https://guide.meteor.com/structure.html
 https://guide.meteor.com/testing.html
